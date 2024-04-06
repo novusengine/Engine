1b6d14dcc257369cbdde4cc863aaf37d23333984
Pursche <pursche01@gmail.com>
2024-04-06 22:25:01 +0200

    Change files to use spaces

--- Source/Base/Base/Util/JsonUtils.h
+++ Source/Base/Base/Util/JsonUtils.h
@@ -7,19 +7,19 @@
 class CVarSystem;
 namespace JsonUtils
 {
-	bool LoadFromPath(nlohmann::json& json, const std::filesystem::path& path);
-	bool LoadFromPathOrCreate(nlohmann::json& json, const nlohmann::json& fallback, const std::filesystem::path& path);
-	bool SaveToPath(const nlohmann::json& json, const std::filesystem::path& path);
+    bool LoadFromPath(nlohmann::json& json, const std::filesystem::path& path);
+    bool LoadFromPathOrCreate(nlohmann::json& json, const nlohmann::json& fallback, const std::filesystem::path& path);
+    bool SaveToPath(const nlohmann::json& json, const std::filesystem::path& path);
 
-	bool LoadFromPath(nlohmann::ordered_json& json, const std::filesystem::path& path);
-	bool LoadFromPathOrCreate(nlohmann::ordered_json& json, const nlohmann::ordered_json& fallback, const std::filesystem::path& path);
-	bool SaveToPath(const nlohmann::ordered_json& json, const std::filesystem::path& path);
+    bool LoadFromPath(nlohmann::ordered_json& json, const std::filesystem::path& path);
+    bool LoadFromPathOrCreate(nlohmann::ordered_json& json, const nlohmann::ordered_json& fallback, const std::filesystem::path& path);
+    bool SaveToPath(const nlohmann::ordered_json& json, const std::filesystem::path& path);
 
-	constexpr u32 CVAR_VERSION = 1u;
-	void VerifyCVarsOrFallback(nlohmann::json& json, const nlohmann::json& fallback);
-	void SaveCVarsToJson(nlohmann::json& json);
-	void LoadCVarsFromJson(nlohmann::json& json);
+    constexpr u32 CVAR_VERSION = 1u;
+    void VerifyCVarsOrFallback(nlohmann::json& json, const nlohmann::json& fallback);
+    void SaveCVarsToJson(nlohmann::json& json);
+    void LoadCVarsFromJson(nlohmann::json& json);
 
-	void SaveCVarsToJson(nlohmann::ordered_json& json);
-	void LoadCVarsFromJson(nlohmann::ordered_json& json);
+    void SaveCVarsToJson(nlohmann::ordered_json& json);
+    void LoadCVarsFromJson(nlohmann::ordered_json& json);
 }
