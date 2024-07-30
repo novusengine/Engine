e95e186f312e4c33d2e0128688b34f4ac53883dc
Foereaper <foereaper@elunatech.com>
2024-07-30 22:46:16 +0200

    Fix Generate build on linux runner

--- Source/Generate/Generate.lua
+++ Source/Generate/Generate.lua
@@ -3,9 +3,12 @@ local currentProject = Solution.Projects.Current
 local dependencies = { }
 Solution.Util.CreateProject("Generate", "StaticLib", currentProject.BinDir, dependencies)
 
-local solutionType = BuildSettings:Get("Solution Type")
-postbuildcommands
-{
-    "cd " .. currentProject.RootDir,
-    "premake5 " .. solutionType
-}
+-- TODO: This needs to be fixed to work properly on Linux. Dirty fix to make runner build properly.
+if os.target() == "windows" then
+    local solutionType = BuildSettings:Get("Solution Type")
+    postbuildcommands
+    {
+        "cd " .. currentProject.RootDir,
+        "premake5 " .. solutionType
+    }
+end
