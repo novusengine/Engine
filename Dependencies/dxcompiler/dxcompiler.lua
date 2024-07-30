47cabd5ff70e8b452a9b60ad718a9818dee2fec4
Foereaper <foereaper@elunatech.com>
2024-07-31 01:01:21 +0200

    Link to dxcompiler globally for Linux

--- Dependencies/dxcompiler/dxcompiler.lua
+++ Dependencies/dxcompiler/dxcompiler.lua
@@ -3,9 +3,9 @@ local mod = Solution.Util.CreateDepTable("dxcompiler", {})
 Solution.Util.CreateDep(mod.Name, mod.Dependencies, function()
     Solution.Util.SetIncludes(mod.Path .. "/include")
     
-    local link = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.lib", mod.Path .. "/lib/linux/libdxcompiler.so")
+    local link = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.lib", "libdxcompiler")
     Solution.Util.SetLinks(link)
 end)
 
-local libPath = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.dll", mod.Path .. "/lib/linux/libdxcompiler.so")
+local libPath = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.dll", "libdxcompiler")
 BuildSettings:Add("DXCompiler Dynamic Lib Path", libPath)
