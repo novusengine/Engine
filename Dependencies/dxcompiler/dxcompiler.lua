b41ec75c5f36bb627cd3157f3920b85467050b7c
Foereaper <foereaper@elunatech.com>
2024-07-31 00:42:23 +0200

    Fix dxcompiler library linking

--- Dependencies/dxcompiler/dxcompiler.lua
+++ Dependencies/dxcompiler/dxcompiler.lua
@@ -1,9 +1,11 @@
 local mod = Solution.Util.CreateDepTable("dxcompiler", {})
-local libPath = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.dll", mod.Path .. "/lib/linux/libdxcompiler.so")
 
 Solution.Util.CreateDep(mod.Name, mod.Dependencies, function()
     Solution.Util.SetIncludes(mod.Path .. "/include")
-    Solution.Util.SetLinks(libPath)
+    
+    local link = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.lib", mod.Path .. "/lib/linux/libdxcompiler.so")
+    Solution.Util.SetLinks(link)
 end)
 
+local libPath = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.dll", mod.Path .. "/lib/linux/libdxcompiler.so")
 BuildSettings:Add("DXCompiler Dynamic Lib Path", libPath)
