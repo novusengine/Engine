a7e1d4a39e856cd9c6c60983258e2ead93272812
Foereaper <foereaper@elunatech.com>
2024-07-30 18:52:57 +0200

    Update dxcompiler premake file

--- Dependencies/dxcompiler/dxcompiler.lua
+++ Dependencies/dxcompiler/dxcompiler.lua
@@ -1,18 +1,9 @@
 local mod = Solution.Util.CreateDepTable("dxcompiler", {})
+local libPath = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.dll", mod.Path .. "/lib/linux/dxcompiler.so")
 
 Solution.Util.CreateDep(mod.Name, mod.Dependencies, function()
     Solution.Util.SetIncludes(mod.Path .. "/include")
-
-    Solution.Util.SetFilter("platforms:Win64", function()
-        local link = mod.Path .. "/lib/windows/dxcompiler.lib"
-        Solution.Util.SetLinks(link)
-    end)
-    
-    Solution.Util.SetFilter("platforms:Linux", function()
-        local link = mod.Path .. "/lib/linux/dxcompiler.so"
-        Solution.Util.SetLinks(link)
-    end)
+    Solution.Util.SetLinks(libPath)
 end)
 
-local libPath = iif(os.istarget("windows"), mod.Path .. "/lib/windows/dxcompiler.dll", mod.Path .. "/lib/linux/dxcompiler.so")
 BuildSettings:Add("DXCompiler Dynamic Lib Path", libPath)
