058f95e08374a24ae6cf356653f47fd166c48ea2
Foereaper <foereaper@elunatech.com>
2024-07-30 21:08:29 +0200

    Remove GLM experimental define

--- Dependencies/glm/glm.lua
+++ Dependencies/glm/glm.lua
@@ -1,7 +1,7 @@
 local dep = Solution.Util.CreateDepTable("glm", {})
 
 Solution.Util.CreateDep(dep.Name, dep.Dependencies, function()
-    local defines = { "GLM_FORCE_LEFT_HANDED", "GLM_FORCE_DEPTH_ZERO_TO_ONE", "GLM_ENABLE_EXPERIMENTAL" }
+    local defines = { "GLM_FORCE_LEFT_HANDED", "GLM_FORCE_DEPTH_ZERO_TO_ONE" }
 
     Solution.Util.SetIncludes(dep.Path)
     Solution.Util.SetDefines(defines)
