143622cb8ecf114177586b22fa91c755f9fab140
NixAJ <nickiarpejensen@gmail.com>
2024-08-10 23:45:27 +0200

    Fix Engine Building on Linux

--- Dependencies/asio/asio.lua
+++ Dependencies/asio/asio.lua
@@ -1,6 +1,6 @@
 local dep = Solution.Util.CreateDepTable("asio", {})
 
 Solution.Util.CreateDep(dep.Name, dep.Dependencies, function()
-    Solution.Util.SetIncludes(dep.Path)
+    Solution.Util.SetIncludes({dep.Path, dep.Path .. "/" .. dep.NameLow})
     Solution.Util.SetDefines({"ASIO_STANDALONE"})
 end)
