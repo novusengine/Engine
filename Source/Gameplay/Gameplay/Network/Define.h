45479e2b9b18d721b3ba3bc1a5d14b55fad7c5d4
Pursche <pursche01@gmail.com>
2025-08-30 16:27:11 +0200

    Add opcodes, defines, generated enums and commands to support the TriggerSystem (#46)

--- Source/Gameplay/Gameplay/Network/Define.h
+++ Source/Gameplay/Gameplay/Network/Define.h
@@ -38,6 +38,8 @@ namespace Network
         SetItemWeaponTemplate,
         SetItemShieldTemplate,
         AddItem,
-        RemoveItem
+        RemoveItem,
+        TriggerAdd,
+        TriggerRemove
     };
 }
